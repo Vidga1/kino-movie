b2b5aa3da1fe85d9174647a7f2cc3c462bbf35ae
vidga471@gmail.com <vidga471@gmail.com>
2025-10-17 02:37:31 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/kino-movies/',
+  base: '/kino-movie/',
   plugins: [tailwindcss(), react()],
 })
